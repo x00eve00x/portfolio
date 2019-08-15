3fceacf4a403a89dbb1410187a331874a863b8f6
Eve Major <major.eve.2426@gmail.com>
2019-08-15 12:34:21 -0400

    Hamburger btn, some JS & btn rotation

---
+++ dist/js/main.js
@@ -0,0 +1,34 @@
+// Select DOM Items
+const hamburgerBtn = document.querySelector('.hamburger-btn');
+const menu = document.querySelector('.menu');
+const menuLeftSide = document.querySelector('.menu--left-side');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.nav-item');
+
+// Set Initial State of Menu
+
+let menuIsVisible = false;
+
+hamburgerBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    // if state of menuIsVisibe isnt false (!), then...
+    if(!menuIsVisible) {
+        hamburgerBtn.classList.add('close');
+        menu.classList.add('show');
+        menuLeftSide.classList.add('show');
+        menuNav.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+        // Set Menu State
+        menuIsVisible = true;
+        
+    } else {
+        hamburgerBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuLeftSide.classList.remove('show');
+        menuNav.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+        // Set Menu State
+        menuIsVisible = false;
+    }
+}
